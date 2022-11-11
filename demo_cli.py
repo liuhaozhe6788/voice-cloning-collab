f0349984786a1cbf8cbd93811e129f0ac7544ecb
liuhaozhe6788 <2792382045@qq.com>
2022-11-11 08:39:14 +0800

    在demo_cli的主循环中，删除异常处理，显示运行错误的位置

--- demo_cli.py
+++ demo_cli.py
@@ -136,161 +136,161 @@ if __name__ == '__main__':
     amp = 1
 
     while True:
-        try:
-            # Get the reference audio filepath
-            message = "Reference voice: enter an audio filepath of a voice to be cloned (mp3, " \
-                      "wav, m4a, flac, ...):\n"
-            in_fpath = Path(input(message).replace("\"", "").replace("\'", ""))
-            fpath_without_ext = os.path.splitext(str(in_fpath))[0]
-            speaker_name = os.path.normpath(fpath_without_ext).split(os.sep)[-1]
+        # try:
+        # Get the reference audio filepath
+        message = "Reference voice: enter an audio filepath of a voice to be cloned (mp3, " \
+                    "wav, m4a, flac, ...):\n"
+        in_fpath = Path(input(message).replace("\"", "").replace("\'", ""))
+        fpath_without_ext = os.path.splitext(str(in_fpath))[0]
+        speaker_name = os.path.normpath(fpath_without_ext).split(os.sep)[-1]
 
-            ## Computing the embedding
-            # First, we load the wav using the function that the speaker encoder provides. This is
-            # important: there is preprocessing that must be applied.
+        ## Computing the embedding
+        # First, we load the wav using the function that the speaker encoder provides. This is
+        # important: there is preprocessing that must be applied.
 
-            # The following two methods are equivalent:
-            # - Directly load from the filepath:
-            # preprocessed_wav = encoder.preprocess_wav(in_fpath)
-            # - If the wav is already loaded:
+        # The following two methods are equivalent:
+        # - Directly load from the filepath:
+        # preprocessed_wav = encoder.preprocess_wav(in_fpath)
+        # - If the wav is already loaded:
 
-            # get duration info from input audio
-            is_wav_file, wav, wav_path = TransFormat(in_fpath, 'wav')
-            # 除了m4a格式无法工作而必须转换以外，无论原格式是否为wav，从稳定性的角度考虑也最好再转为wav（因为某些wav本身不带比特率属性，无法在此代码中工作，因此需要转换以赋予其该属性）
-            path_ori, filename_ori = os.path.split(wav_path)
-            totDur_ori, nPause_ori, arDur_ori, nSyl_ori, arRate_ori = AudioAnalysis(path_ori, filename_ori)
-            DelFile(path_ori, '.TextGrid')
+        # get duration info from input audio
+        is_wav_file, wav, wav_path = TransFormat(in_fpath, 'wav')
+        # 除了m4a格式无法工作而必须转换以外，无论原格式是否为wav，从稳定性的角度考虑也最好再转为wav（因为某些wav本身不带比特率属性，无法在此代码中工作，因此需要转换以赋予其该属性）
+        path_ori, filename_ori = os.path.split(wav_path)
+        totDur_ori, nPause_ori, arDur_ori, nSyl_ori, arRate_ori = AudioAnalysis(path_ori, filename_ori)
+        DelFile(path_ori, '.TextGrid')
 
-            if not is_wav_file:
-                os.remove(wav_path)  # remove intermediate wav files
-            
-            preprocessed_wav = encoder.preprocess_wav(wav)
+        if not is_wav_file:
+            os.remove(wav_path)  # remove intermediate wav files
+        
+        preprocessed_wav = encoder.preprocess_wav(wav)
 
-            print("Loaded input audio file succesfully")
+        print("Loaded input audio file succesfully")
 
-            # Then we derive the embedding. There are many functions and parameters that the
-            # speaker encoder interfaces. These are mostly for in-depth research. You will typically
-            # only use this function (with its default parameters):
-            input_embed = encoder.embed_utterance(preprocessed_wav)
+        # Then we derive the embedding. There are many functions and parameters that the
+        # speaker encoder interfaces. These are mostly for in-depth research. You will typically
+        # only use this function (with its default parameters):
+        input_embed = encoder.embed_utterance(preprocessed_wav)
 
-            # Choose standard audio
+        # Choose standard audio
 
-            fft_max_freq = vocoder.get_dominant_freq(preprocessed_wav)
-            print(f"\nthe dominant frequency of input audio is {fft_max_freq}Hz")
-            if fft_max_freq < split_freq:
-                standard_fpath = "standard_audios/male_1.wav"
-            else:
-                standard_fpath = "standard_audios/female_1.wav"
+        fft_max_freq = vocoder.get_dominant_freq(preprocessed_wav)
+        print(f"\nthe dominant frequency of input audio is {fft_max_freq}Hz")
+        if fft_max_freq < split_freq:
+            standard_fpath = "standard_audios/male_1.wav"
+        else:
+            standard_fpath = "standard_audios/female_1.wav"
 
-            standard_wav = Synthesizer.load_preprocess_wav(standard_fpath)
-            preprocessed_standard_wav = encoder.preprocess_wav(standard_wav)
-            print("Loaded standard audio file succesfully")
+        standard_wav = Synthesizer.load_preprocess_wav(standard_fpath)
+        preprocessed_standard_wav = encoder.preprocess_wav(standard_wav)
+        print("Loaded standard audio file succesfully")
 
-            standard_embed = encoder.embed_utterance(preprocessed_standard_wav)
+        standard_embed = encoder.embed_utterance(preprocessed_standard_wav)
 
-            embed1=input_embed.dot(weight)
-            embed2=standard_embed.dot(1 - weight)
-            embed=embed1+embed2
+        embed1=input_embed.dot(weight)
+        embed2=standard_embed.dot(1 - weight)
+        embed=embed1+embed2
 
-            embed[embed < set_zero_thres]=0 # 噪声值置零
-            embed = embed * amp
+        embed[embed < set_zero_thres]=0 # 噪声值置零
+        embed = embed * amp
 
-            ## Generating the spectrogram
-            text = input("Write a sentence to be synthesized:\n")
+        ## Generating the spectrogram
+        text = input("Write a sentence to be synthesized:\n")
 
-            # If seed is specified, reset torch seed and force synthesizer reload
-            if args.seed is not None:
-                torch.manual_seed(args.seed)
-                synthesizer = Synthesizer(args.syn_model_fpath)
-            
-            import re
-            # The synthesizer works in batch, so you need to put your data in a list or numpy array
-            def split_text(text):
-                def convert(match_obj):
-                    if match_obj.group() is not None:
-                        return match_obj.group().replace(',', '')
-                text = re.sub(r"[0-9]+[\,][0-9]+", convert, text)
-                text = text.replace('-', ' ')
-                text = text.replace(',', '.')
-                text = text.replace(';', '.')
-                text = text.replace(':', '.')
-                    
-                texts = [i.text.strip() for i in nlp(text).sents]  # split paragraph to sentences
-                return texts
-            texts = split_text(text)
-            print(f"the list of inputs texts:\n{texts}")
+        # If seed is specified, reset torch seed and force synthesizer reload
+        if args.seed is not None:
+            torch.manual_seed(args.seed)
+            synthesizer = Synthesizer(args.syn_model_fpath)
+        
+        import re
+        # The synthesizer works in batch, so you need to put your data in a list or numpy array
+        def split_text(text):
+            def convert(match_obj):
+                if match_obj.group() is not None:
+                    return match_obj.group().replace(',', '')
+            text = re.sub(r"[0-9]+[\,][0-9]+", convert, text)
+            text = text.replace('-', ' ')
+            text = text.replace(',', '.')
+            text = text.replace(';', '.')
+            text = text.replace(':', '.')
+                
+            texts = [i.text.strip() for i in nlp(text).sents]  # split paragraph to sentences
+            return texts
+        texts = split_text(text)
+        print(f"the list of inputs texts:\n{texts}")
 
-            embeds = [embed] * len(texts)
-            # If you know what the attention layer alignments are, you can retrieve them here by
-            # passing return_alignments=True
-            specs, alignments = synthesizer.synthesize_spectrograms(texts, embeds, return_alignments=True)
-            save_attention(alignments.detach().cpu().numpy()[-1, :, :], "attention")
+        embeds = [embed] * len(texts)
+        # If you know what the attention layer alignments are, you can retrieve them here by
+        # passing return_alignments=True
+        specs, alignments = synthesizer.synthesize_spectrograms(texts, embeds, return_alignments=True)
+        save_attention(alignments.detach().cpu().numpy()[-1, :, :], "attention")
 
-            breaks = [spec.shape[1] for spec in specs]
-            spec = np.concatenate(specs, axis=1)
-            print("Created the mel spectrogram")
-
-
-            ## Generating the waveform
-            print("Synthesizing the waveform:")
-
-            # If seed is specified, reset torch seed and reload vocoder
-            if args.seed is not None:
-                torch.manual_seed(args.seed)
-                vocoder.load_model(args.voc_model_fpath)
-
-            # Synthesizing the waveform is fairly straightforward. Remember that the longer the
-            # spectrogram, the more time-efficient the vocoder.
-            if not args.griffin_lim:
-                wav = vocoder.infer_waveform(spec)
-            else:
-                wav = Synthesizer.griffin_lim(spec)
-
-            wav = vocoder.waveform_denoising(wav)
-
-            # Add breaks
-            b_ends = np.cumsum(np.array(breaks) * Synthesizer.hparams.hop_size)
-            b_starts = np.concatenate(([0], b_ends[:-1]))
-            wavs = [wav[start:end] for start, end, in zip(b_starts, b_ends)]
-            breaks = [np.zeros(int(0.15 * Synthesizer.sample_rate))] * len(breaks)
-            wav = np.concatenate([i for w, b in zip(wavs, breaks) for i in (w, b)])
-
-            # Trim excess silences to compensate for gaps in spectrograms (issue #53)
-            # generated_wav = encoder.preprocess_wav(generated_wav)
-            wav = wav / np.abs(wav).max() * 1
-
-            # Save it on the disk
-            # filename = "demo_output_%02d.wav" % num_generated
-            if not os.path.exists("out_audios"):
-                os.mkdir("out_audios")
-            filename = f"out_audios/{speaker_name}_syn.wav"
-            # print(wav.dtype)
-            sf.write(filename, wav.astype(np.float32), synthesizer.sample_rate)
-            num_generated += 1
-            print("\nSaved output (havent't change speed) as %s\n\n" % filename)
-
-            # Fix Speed(generate new audio)
-            fix_file = work(totDur_ori, 
-                            nPause_ori, 
-                            arDur_ori, 
-                            nSyl_ori, 
-                            arRate_ori, 
-                            filename)
-            print(f"\nSaved output (fixed speed) as {fix_file}\n\n")
+        breaks = [spec.shape[1] for spec in specs]
+        spec = np.concatenate(specs, axis=1)
+        print("Created the mel spectrogram")
 
 
-            # # Play the audio (non-blocking)
-            # if not args.no_sound:
-            #     import sounddevice as sd
-            #     try:
-            #         sd.stop()
-            #         sd.play(wav, synthesizer.sample_rate)
-            #     except sd.PortAudioError as e:
-            #         print("\nCaught exception: %s" % repr(e))
-            #         print("Continuing without audio playback. Suppress this message with the \"--no_sound\" flag.\n")
-            #     except:
-            #         raise
+        ## Generating the waveform
+        print("Synthesizing the waveform:")
+
+        # If seed is specified, reset torch seed and reload vocoder
+        if args.seed is not None:
+            torch.manual_seed(args.seed)
+            vocoder.load_model(args.voc_model_fpath)
+
+        # Synthesizing the waveform is fairly straightforward. Remember that the longer the
+        # spectrogram, the more time-efficient the vocoder.
+        if not args.griffin_lim:
+            wav = vocoder.infer_waveform(spec)
+        else:
+            wav = Synthesizer.griffin_lim(spec)
+
+        wav = vocoder.waveform_denoising(wav)
+
+        # Add breaks
+        b_ends = np.cumsum(np.array(breaks) * Synthesizer.hparams.hop_size)
+        b_starts = np.concatenate(([0], b_ends[:-1]))
+        wavs = [wav[start:end] for start, end, in zip(b_starts, b_ends)]
+        breaks = [np.zeros(int(0.15 * Synthesizer.sample_rate))] * len(breaks)
+        wav = np.concatenate([i for w, b in zip(wavs, breaks) for i in (w, b)])
+
+        # Trim excess silences to compensate for gaps in spectrograms (issue #53)
+        # generated_wav = encoder.preprocess_wav(generated_wav)
+        wav = wav / np.abs(wav).max() * 1
+
+        # Save it on the disk
+        # filename = "demo_output_%02d.wav" % num_generated
+        if not os.path.exists("out_audios"):
+            os.mkdir("out_audios")
+        filename = f"out_audios/{speaker_name}_syn.wav"
+        # print(wav.dtype)
+        sf.write(filename, wav.astype(np.float32), synthesizer.sample_rate)
+        num_generated += 1
+        print("\nSaved output (havent't change speed) as %s\n\n" % filename)
+
+        # Fix Speed(generate new audio)
+        fix_file = work(totDur_ori, 
+                        nPause_ori, 
+                        arDur_ori, 
+                        nSyl_ori, 
+                        arRate_ori, 
+                        filename)
+        print(f"\nSaved output (fixed speed) as {fix_file}\n\n")
 
 
-        except Exception as e:
-            print("Caught exception: %s" % repr(e))
-            print("Restarting\n")
+        # # Play the audio (non-blocking)
+        # if not args.no_sound:
+        #     import sounddevice as sd
+        #     try:
+        #         sd.stop()
+        #         sd.play(wav, synthesizer.sample_rate)
+        #     except sd.PortAudioError as e:
+        #         print("\nCaught exception: %s" % repr(e))
+        #         print("Continuing without audio playback. Suppress this message with the \"--no_sound\" flag.\n")
+        #     except:
+        #         raise
+
+
+        # except Exception as e:
+        #     print("Caught exception: %s" % repr(e))
+        #     print("Restarting\n")
