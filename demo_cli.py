11a4045b06e0cc88151bf3ee65ffa31d2e2bc67c
liuhaozhe6788 <2792382045@qq.com>
2023-06-09 17:49:19 +0800

    denoise using fb denoiser

--- demo_cli.py
+++ demo_cli.py
@@ -236,7 +236,7 @@ if __name__ == '__main__':
         start_syn = time.time()
         # Generating the spectrogram
         # text = input("Write a sentence to be synthesized:\n")
-        text = "The North Wind and the Sun were disputing which was the stronger, when a traveler came along wrapped in a warm cloak. They agreed that the one who first succeeded in making the traveler take his cloak off should be considered stronger than the other. Then the North Wind blew as hard as he could, but the more he blew the more closely did the traveler fold his cloak around him; and at last the North Wind gave up the attempt. Then the Sun shined out warmly, and immediately the traveler took off his cloak.And so the North Wind was obliged to confess that the Sun was the stronger of the two."
+        text = "Mechanics is an essential branch of physics that provides a framework for understanding the behavior of physical bodies under the influence of various forces. The principles of mechanics are based on the laws of motion, which form the foundation of the field. Mechanics has many practical applications in engineering and technology, from aerospace and automotive engineering to robotics and manufacturing. As science and technology continue to evolve, the principles of mechanics will remain an important part of our understanding of the physical world."
 
         # If seed is specified, reset torch seed and force synthesizer reload
         if args.seed is not None:
@@ -307,7 +307,7 @@ if __name__ == '__main__':
         wav = np.concatenate([i for w, b in zip(wavs, breaks) for i in (w, b)])
 
         # Trim excess silences to compensate for gaps in spectrograms (issue #53)
-        generated_wav = encoder.inference.preprocess_wav(wav)
+        # generated_wav = encoder.inference.preprocess_wav(wav)
         wav = wav / np.abs(wav).max() * 4
 
         # Save it on the disk
