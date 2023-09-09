6169f3deec8138b2acaeb2a4c03e819608af5098
liuhaozhe6788 <2792382045@qq.com>
2023-09-09 13:01:33 +0800

    new commits

--- demo_cli.py
+++ demo_cli.py
@@ -216,8 +216,7 @@ if __name__ == '__main__':
 
         start_syn = time.time()
         # Generating the spectrogram
-        # text = input("Write a sentence to be synthesized:\n")
-        text = "Mechanics is a branch of physics that deals with the behavior of physical bodies under the influence of various forces. The study of mechanics is important in understanding the behavior of machines, the motion of objects, and the principles of engineering. Mechanics has been an essential part of physics since ancient times and has continued to evolve with advancements in science and technology. This paper will discuss the principles of mechanics, the laws of motion, and the applications of mechanics in engineering and technology."
+        text = input("Write a sentence to be synthesized:\n")
 
         # If seed is specified, reset torch seed and force synthesizer reload
         if args.seed is not None:
